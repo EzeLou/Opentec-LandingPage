34ff8b93611083c96444af5e5473671413dd5b6c
EzeLou <ezequielmatiaslourenco@gmail.com>
2021-09-01 18:16:32 -0300

    php

--- old.php
+++ old.php
@@ -1,10 +1,10 @@
 <?php 
 
 // database connection code
-//if(isset($_POST['first_name']))
-//{
+if(isset($_POST['first_name']))
+{
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
-$con = mysqli_connect('127.0.0.1:3306', 'u742194444_OPENTEC', 'deftFlamink2021','u742194444_contact_form');
+$con = mysqli_connect('156.67.72.104:443', 'u742194444_OPENTEC', 'deftFlamink2021','u742194444_contact_form');
 
 // check connection
 if(!$con){
@@ -20,7 +20,7 @@ $email = $_POST['email'];
 $description = $_POST['description'];
 
 // database insert SQL code
-$sql = "INSERT INTO 'contacts' ('contact_id','first_name','last_name','phone','email','description') VALUES ('0', '$first_name', '$last_name', '$phone', '$email', '$description')";
+$sql = "INSERT INTO 'tbl_contact' ('contact_id','first_name','last_name','phone','email','description') VALUES ('0', '$first_name', '$last_name', '$phone', '$email', '$description')";
 echo"lol";
 // insert in database 
 $rs = mysqli_query($con, $sql);
@@ -28,7 +28,7 @@ if($rs)
 {
 	echo "Contact Records Inserted";
 }
-//}
+}
 else
 {
 	echo "Are you a genuine visitor?";
