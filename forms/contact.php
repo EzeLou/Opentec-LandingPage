aab1629ce884dbb8dd15b535c954bbcf056d5a99
ThiagoMariuzza <thiagonicomb.xd@gmail.com>
2021-09-14 19:03:37 -0300

    Final Changess BOOYA !!

--- forms/contact.php
+++ forms/contact.php
@@ -25,10 +25,12 @@ $email_subject = "Nueva Consulta OPENTEC";
 $email_body = '<table><tr><th>Nombre: </th><th>'.$first_name.' '.$last_name.'</th></tr><tr><th>Telefono: </th><th>'.$phone.'</th></tr><tr><th>Email: </th><th>'.$email.'</th></tr><tr><th>Consulta: </th><th>'.$description.'</th></tr></table>';
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
-$headers .= 'From: Contacto-Pagina@opentec.com' . "\r\n" . 'Reply-To: ezenoleintereza@example.com' . "\r\n";
+$headers .= 'From: Contacto-Pagina@opentec.com' . "\r\n" . 'Reply-To: info@opentec.com.ar' . "\r\n";
 
 mail($email_to,$email_subject,$email_body,$headers);
+header( 'Location: https://deftflamink.com/test/');
 
+https://deftflamink.com/test/
 // database insert SQL code
 $sql = "INSERT INTO contacts (first_name,last_name,phone,email,description) VALUES ('$first_name', '$last_name', '$phone', '$email', '$description')";
 echo $sql;
